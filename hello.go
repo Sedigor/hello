34cf2e7884592e051b28639945f9fe6182864858
Ihor Siedielnikov <sisemail92@gmail.com>
2022-01-10 20:55:04 +0200

    second

--- hello.go
+++ hello.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
-	fmt.Println("new")
+	answer := "mmm"
+	a, b := yesNo(answer)
+	fmt.Println(a)
+	fmt.Println(b)
+}
+
+func yesNo(answer string) (bool, error) {
+	if answer == "yes" {
+		return true, nil
+	} else if answer == "no" {
+		return true, nil
+	}
+	return false, errors.New("this is not Yes or No")
 }
